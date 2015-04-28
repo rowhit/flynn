f0fee258b0f924eccc4dfd5a63685a33d0c0c94a
Jesse Stuart <jesse@jessestuart.ca>
2015-04-27 20:08:01 -0400

    refactor: move clusters list into own component
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- installer/app/src/views/main.js.jsx
+++ installer/app/src/views/main.js.jsx
@@ -1,8 +1,6 @@
 import UserAgent from './css/user-agent';
-import Panel from './panel';
-import { List, ListItem } from './list';
 import { extend } from 'marbles/utils';
-import Dispatcher from '../dispatcher';
+import Clusters from './clusters';
 
 var Main = React.createClass({
 	getDefaultProps: function () {
@@ -19,66 +17,10 @@ var Main = React.createClass({
 	},
 
 	render: function () {
-		var currentClusterID = this.state.currentClusterID;
-		var clusters = this.state.clusters;
 		return (
 			<div style={this.props.css}>
-				<div style={extend({}, this.props.childrenCSS, { marginRight: 16, maxWidth: 360, minWidth: 300 })}>
-					<Panel style={{ height: '100%' }}>
-						<h2>Clusters</h2>
-
-						<List>
-							<ListItem selected={currentClusterID === null} path="/">New</ListItem>
-
-							{clusters.map(function (cluster) {
-								var installState = cluster.getInstallState();
-								return (
-									<ListItem
-										key={cluster.ID}
-										selected={currentClusterID === cluster.ID}
-										path={'/clusters/:id'}
-										params={[{ id: cluster.ID }]}
-										innerStyle={{
-											display: UserAgent.isSafari() ? '-webkit-flex' : 'flex'
-										}}>
-
-										<div style={{
-											flexGrow: 1,
-											WebkitFlexGrow: 1
-										}}>{cluster.name}</div>
-										<div>
-											{installState.prompt !== null ? (
-												<span>
-													<span className="fa fa-bell" />
-													&nbsp;
-												</span>
-											) : null}
-
-											{installState.currentStep === 'install' && !installState.failed ? (
-												<span>
-													<span className="fa fa-cog fa-spin" />
-													&nbsp;
-												</span>
-											) : null}
-
-											{installState.failed ? (
-												<span>
-													<span className="fa fa-exclamation-triangle" />
-													&nbsp;
-												</span>
-											) : null}
-
-											<span className={"fa "+ (installState.deleting ? "fa-eye-slash" : "fa-trash")} onClick={function (e) {
-												e.preventDefault();
-												e.stopPropagation();
-												this.__handleClusterDeleteBtnClick(cluster);
-											}.bind(this)} />
-										</div>
-									</ListItem>
-								);
-							}.bind(this))}
-						</List>
-					</Panel>
+				<div style={extend({}, this.props.childrenCSS, { marginRight: 16, maxWidth: 360, minWidth: 300, flexBasis: 360 })}>
+					<Clusters dataStore={this.props.dataStore} />
 				</div>
 
 				<div style={this.props.childrenCSS}>
@@ -86,40 +28,6 @@ var Main = React.createClass({
 				</div>
 			</div>
 		);
-	},
-
-	componentDidMount: function () {
-		this.props.dataStore.addChangeListener(this.__handleDataChange);
-	},
-
-	componentWillUnmount: function () {
-		this.props.dataStore.removeChangeListener(this.__handleDataChange);
-	},
-
-	getInitialState: function () {
-		return this.__getState();
-	},
-
-	__getState: function () {
-		return this.props.dataStore.state;
-	},
-
-	__handleDataChange: function () {
-		this.setState(this.__getState());
-	},
-
-	__handleClusterDeleteBtnClick: function (cluster) {
-		if (cluster.getInstallState().deleting) {
-			Dispatcher.dispatch({
-				name: 'CONFIRM_CLUSTER_DELETE',
-				clusterID: cluster.ID
-			});
-		} else {
-			Dispatcher.dispatch({
-				name: 'CLUSTER_DELETE',
-				clusterID: cluster.ID
-			});
-		}
 	}
 });
 export default Main;
